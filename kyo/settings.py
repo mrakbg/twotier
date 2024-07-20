9b609ceee770fedb976552137a57ae0ded25024c
Anuj Gupta <anujguptaa1234567@gmail.com>
2024-07-20 14:18:00 +0530

    Update settings.py

--- kyo/settings.py
+++ kyo/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'kyo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'kyo_database',
-        'USER': 'root',
-        'PASSWORD': '**************',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'kyo_database',
+#         'USER': 'root',
+#         'PASSWORD': '**************',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 
 
